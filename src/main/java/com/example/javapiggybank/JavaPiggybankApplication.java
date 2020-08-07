49867a1433d4a400e95de2aa083c8e0ef72d7ea5
ViridityMoon <viriditymoon@gmail.com>
2020-08-07 13:29:19 -0400

    Finished MVP!

---
+++ src/main/java/com/example/javapiggybank/JavaPiggybankApplication.java
@@ -0,0 +1,13 @@
+package com.example.javapiggybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPiggybankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaPiggybankApplication.class, args);
+    }
+
+}
